84cef38e7ea163341c7b14469e591e341d243511
Full-Stack-boi <151743553+Full-Stack-boi@users.noreply.github.com>
2023-12-10 22:04:19 +0700

    Update NavBar.tsx
    แก้ code ให้ถูก

--- app/components/nav/NavBar.tsx
+++ app/components/nav/NavBar.tsx
@@ -5,10 +5,10 @@ const textlogoBanner = Kdam_Thmor_Pro({ subsets: ["latin"], weight: ["400"] });
 
 const NavBar = () => {
   return (
-    <div className="sticky top-0 w-Full bg-slate-200 z-30 shadow-sm">
+    <div className="sticky top-0 w-full bg-slate-200 z-30 shadow-sm">
       <div className="py-4 border-b-[1px]">
         <Container>
-          <div className="flex items-center justify-between gap-3 md-gap-0">
+          <div className="flex items-center justify-between gap-3 md:gap-0">
             <Link
               href="/"
               className={`${textlogoBanner.className} font-bold text-2xl`}
@@ -16,7 +16,7 @@ const NavBar = () => {
               Mechamongus
             </Link>
             <div className="hidden md:block">Search</div>
-            <div className="flex items-center gap-8 md-gap-12">
+            <div className="flex items-center gap-8 md:gap-12">
               <div>Cart Count</div>
               <div>UserMenu</div>
             </div>
