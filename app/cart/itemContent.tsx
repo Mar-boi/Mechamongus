c71d5e023331f2d06f797a63acf44ca2ba8dfebf
Full-Stack-boi <151743553+Full-Stack-boi@users.noreply.github.com>
2023-12-13 01:36:08 +0700

    แก้บัต format ไม่ตรงหัวข้อ : itemContent.tsx
    - md:tex-sm เป็น md:text-sm
    - border-slate-2 เป็น border-slate-200
    - แก้ใส่ div มาเกิน
    - ใส่ pr-2 ให้ เพื่อคส่มสวยงาม (ที่ราคารวม)

--- app/cart/itemContent.tsx
+++ app/cart/itemContent.tsx
@@ -5,39 +5,46 @@ import { truncateText } from "@/utils/truncateText";
 import Image from "next/image";
 import SetQuatity from "../components/products/SetQuantity";
 
-interface ItemContentProps{
-    item: CartProductType
+interface ItemContentProps {
+  item: CartProductType;
 }
 
-const ItemContent: React.FC<ItemContentProps> = ({item }) => {
-    return <div className="grid grid-cols-5 text-xs md:tex-sm gap-4 border-[1.5px] border-slate-2 py-4 items-center">
-        <div className="col-span-2 justify-self-start flex gap-2 md:gap-4"><Link href={`/product/${item.id}`}>
-            <div className="relative w-[90px] aspect-square">
-                <Image src={item.selectedImg.image}
-                alt={item.name}
-                fill
-                className="object-contain"/></div>
-            
-        </Link></div>
-        <div className="flex flex-col justify-between">
-            <Link href={`/product/${item.id}`}>
-                {truncateText(item.name)}</Link>
-            <div>{item.selectedImg.color}</div>
-            <div className="w-[700px]">
-                <button className="text-slate-500 underline" onClick={() => {}}>Remove
-                </button>
-            </div>
+const ItemContent: React.FC<ItemContentProps> = ({ item }) => {
+  return (
+    <div className="grid grid-cols-5 text-xs md:text-sm gap-4 border-[1.5px] border-slate-200 py-4 items-center">
+      <Link href={`/product/${item.id}`}>
+        <div className="relative w-[90px] aspect-square">
+          <Image
+            src={item.selectedImg.image}
+            alt={item.name}
+            fill
+            className="object-contain"
+          />
         </div>
-        <div className="justify-self-center">{formatPrice(item.price)}</div>
-        <div className="justify-self-center">
-            <SetQuatity cartCounter={true} 
-            cartProduct={item} handleQtyIncrease={() =>{}}
-            handleQtyDecrease={() => {}}/>
+      </Link>
+      <div className="flex flex-col justify-between">
+        <Link href={`/product/${item.id}`}>{truncateText(item.name)}</Link>
+        <div>{item.selectedImg.color}</div>
+        <div className="w-[70px]">
+          <button className="text-slate-500 underline" onClick={() => {}}>
+            Remove
+          </button>
         </div>
-        <div className="justify-self-end font-semibold">
-            {formatPrice(item.price * item.quantity)}
-        </div>
-    </div>;
-}
+      </div>
+      <div className="justify-self-center">{formatPrice(item.price)}</div>
+      <div className="justify-self-center">
+        <SetQuatity
+          cartCounter={true}
+          cartProduct={item}
+          handleQtyIncrease={() => {}}
+          handleQtyDecrease={() => {}}
+        />
+      </div>
+      <div className="justify-self-end font-semibold pr-2">
+        {formatPrice(item.price * item.quantity)}
+      </div>
+    </div>
+  );
+};
 
 export default ItemContent;
