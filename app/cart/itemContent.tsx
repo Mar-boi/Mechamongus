5aa20bfd6e106a540a2da6e74404354a33bf8ce0
Full-Stack-boi <151743553+Full-Stack-boi@users.noreply.github.com>
2023-12-13 02:00:59 +0700

    แก้บัต format ไม่ตรงหัวข้อ ครั้งที่ 2
    แก้ div แล้ว แต่เทคปิดอยู่ผิดที่

--- app/cart/itemContent.tsx
+++ app/cart/itemContent.tsx
@@ -12,23 +12,26 @@ interface ItemContentProps {
 const ItemContent: React.FC<ItemContentProps> = ({ item }) => {
   return (
     <div className="grid grid-cols-5 text-xs md:text-sm gap-4 border-[1.5px] border-slate-200 py-4 items-center">
-      <Link href={`/product/${item.id}`}>
-        <div className="relative w-[90px] aspect-square">
-          <Image
-            src={item.selectedImg.image}
-            alt={item.name}
-            fill
-            className="object-contain"
-          />
-        </div>
-      </Link>
-      <div className="flex flex-col justify-between">
-        <Link href={`/product/${item.id}`}>{truncateText(item.name)}</Link>
-        <div>{item.selectedImg.color}</div>
-        <div className="w-[70px]">
-          <button className="text-slate-500 underline" onClick={() => {}}>
-            Remove
-          </button>
+      <div className="col-span-2 justify-self-start flex gap-2 md:gap-4">
+        <Link href={`/product/${item.id}`}>
+          <div className="relative w-[90px] aspect-square">
+            <Image
+              src={item.selectedImg.image}
+              alt={item.name}
+              fill
+              className="object-contain"
+            />
+          </div>
+        </Link>
+
+        <div className="flex flex-col justify-between">
+          <Link href={`/product/${item.id}`}>{truncateText(item.name)}</Link>
+          <div>{item.selectedImg.color}</div>
+          <div className="w-[70px]">
+            <button className="text-slate-500 underline" onClick={() => {}}>
+              Remove
+            </button>
+          </div>
         </div>
       </div>
       <div className="justify-self-center">{formatPrice(item.price)}</div>
@@ -40,6 +43,7 @@ const ItemContent: React.FC<ItemContentProps> = ({ item }) => {
           handleQtyDecrease={() => {}}
         />
       </div>
+
       <div className="justify-self-end font-semibold pr-2">
         {formatPrice(item.price * item.quantity)}
       </div>
