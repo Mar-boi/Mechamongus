7a83235ede2080cc03371879d6ded1e42eeec707
Full-Stack-boi <151743553+Full-Stack-boi@users.noreply.github.com>
2023-12-13 03:13:52 +0000

    แก้ Error CartProvider.tsx

--- provider/CartPovider.tsx
+++ provider/CartPovider.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { CartContextProvider } from "@/hook/useCart";
+import { CartContextProvider } from "@/hooks/useCart";
 
 interface CartProviderProps {
   children: React.ReactNode;
