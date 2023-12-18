1e706eec04b607703aae520956d4dcdd9b059e37
Full-Stack-boi <151743553+Full-Stack-boi@users.noreply.github.com>
2023-12-18 10:32:13 +0700

    Update formatPrice.ts :white_check_mark:
    - แก้ให้แสดงค่าเงินดูง่าย

--- utils/formatPrice.ts
+++ utils/formatPrice.ts
@@ -2,5 +2,6 @@ export const formatPrice = (amount: number) => {
   return new Intl.NumberFormat("th-TH", {
     style: "currency",
     currency: "THB",
+    minimumFractionDigits: 0,
   }).format(amount);
 };
