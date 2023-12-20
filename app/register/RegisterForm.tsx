626f62ed90c0df1d4d7bcea967a7353011f9faee
Full-Stack-boi <151743553+Full-Stack-boi@users.noreply.github.com>
2023-12-20 22:06:34 +0700

    เก็บข้อมูล register ลง Database :white_check_mark:
    - สร้าง Database มีตำแน่ง User กับ Admin
    - เชื่อม Database
    - กดสร้างบัญชี
    - เก็บข้อมูล บัญชี ลง Database

--- app/register/RegisterForm.tsx
+++ app/register/RegisterForm.tsx
@@ -7,6 +7,10 @@ import { FieldValues, useForm, SubmitHandler } from "react-hook-form";
 import Button from "../components/Button";
 import Link from "next/link";
 import { AiOutlineGoogle } from "react-icons/ai";
+import axios from "axios";
+import toast from "react-hot-toast";
+import { signIn } from "next-auth/react";
+import { useRouter } from "next/navigation";
 
 const RegisterForm = () => {
   const [isLoading, setIsLoading] = useState(false);
@@ -22,9 +26,36 @@ const RegisterForm = () => {
     },
   });
 
+  const router = useRouter();
+
   const onsubmit: SubmitHandler<FieldValues> = (data) => {
     setIsLoading(true);
-    console.log(data);
+
+    axios
+      .post("/api/register", data)
+      .then(() => {
+        toast.success("Account created");
+
+        signIn("credentials", {
+          email: data.email,
+          password: data.password,
+          redirect: false,
+        }).then((callback) => {
+          if (callback?.ok) {
+            router.push("/cart");
+            router.refresh();
+            toast.success("Logged In");
+          }
+
+          if (callback?.error) {
+            toast.error(callback.error);
+          }
+        });
+      })
+      .catch(() => toast.error("Something went wrong"))
+      .finally(() => {
+        setIsLoading(false);
+      });
   };
 
   return (
@@ -38,7 +69,7 @@ const RegisterForm = () => {
       />
       <hr className=" bg-slate-300 w-full h-px" />
       <Input
-        id="Name"
+        id="name"
         label="Name"
         disabled={isLoading}
         register={register}
