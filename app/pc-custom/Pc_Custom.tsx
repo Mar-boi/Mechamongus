a68058314f9b84f435c340a6300e679030e2c62f
Full-Stack-boi <151743553+Full-Stack-boi@users.noreply.github.com>
2024-02-11 18:22:38 +0700

    Update Pc_Custom.tsx

--- app/pc-custom/Pc_Custom.tsx
+++ app/pc-custom/Pc_Custom.tsx
@@ -1,9 +1,11 @@
 "use client";
 
-const Pc_buildMain = () => {
+const Pc_Custom = () => {
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 gap-12    max-w-[1920px]    mx-auto   xl:px-20   md:px-2   px-4 mt-5">
-      <div>Test</div>
+      <div className="relative  h-auto overflow-hidden rounded-xl border border-gray-200 bg-white shadow-md ">
+        Test
+      </div>
       <div className="relative  h-auto overflow-hidden rounded-xl border border-gray-200 bg-white shadow-md ">
         Test 2
       </div>
@@ -11,4 +13,4 @@ const Pc_buildMain = () => {
   );
 };
 
-export default Pc_buildMain;
+export default Pc_Custom;
