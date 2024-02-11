09ae5c299c242330a7195be2f4b22897278cd123
Full-Stack-boi <151743553+Full-Stack-boi@users.noreply.github.com>
2024-02-11 18:22:08 +0700

    เพิ่มหน้า จัดสเปครอ

---
+++ app/pc-custom/Pc_Custom.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+const Pc_buildMain = () => {
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 gap-12    max-w-[1920px]    mx-auto   xl:px-20   md:px-2   px-4 mt-5">
+      <div>Test</div>
+      <div className="relative  h-auto overflow-hidden rounded-xl border border-gray-200 bg-white shadow-md ">
+        Test 2
+      </div>
+    </div>
+  );
+};
+
+export default Pc_buildMain;
---
+++ app/pc-custom/page.tsx
@@ -0,0 +1,12 @@
+import Container from "../components/Container";
+import Pc_buildMain from "./Pc_build";
+
+const Pc_build = () => {
+  return (
+    <Container>
+      <Pc_buildMain></Pc_buildMain>
+    </Container>
+  );
+};
+
+export default Pc_build;
